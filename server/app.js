7d8f702a091665ce9bf605759e4253c2553956bd
Alex <31377763+AlexH893@users.noreply.github.com>
2021-11-13 12:28:55 -0700

    fixes

--- server/app.js
+++ server/app.js
@@ -1,17 +1,14 @@
 /**
  * Require statements
  */
-require('dotenv').load();
-const createError = require('http-errors');
-const express = require('express');
-const http = require('http');
-const morgan = require('morgan');
-const bodyParser = require('body-parser');
-const passport = require('passport');
-
-require('./routes/config/passport');
-const path = require('path');
-const mongoose = require('mongoose');
+require("dotenv").config({ path: "../db.env" });
+const createError = require("http-errors");
+const express = require("express");
+const http = require("http");
+const morgan = require("morgan");
+const bodyParser = require("body-parser");
+const path = require("path");
+const mongoose = require("mongoose");
 const Router = express.Router();
 const Employee = require("./models/employee.js");
 let empRoutes = require("./routes/employee-routes.js");
@@ -21,43 +18,41 @@ let empRoutes = require("./routes/employee-routes.js");
  */
 let app = express();
 app.use(bodyParser.json());
-app.use(bodyParser.urlencoded({'extended': true}));
-app.use(morgan('dev'));
-app.use(express.static(path.join(__dirname, '../dist/nodebucket')));
-app.use('/', express.static(path.join(__dirname, '../dist/nodebucket')));
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(morgan("dev"));
+app.use(express.static(path.join(__dirname, "../dist/nodebucket")));
+app.use("/", express.static(path.join(__dirname, "../dist/nodebucket")));
 
 /**
  * Variables
  */
 const port = 3000; // server port
 
-// conncection string
-var conn = "mongodb://localhost:27017/?readPreference=primary&appname=MongoDB%20Compass&directConnection=true&ssl=false";
-
 /**
  * Database connection
  */
-mongoose.connect(conn, {
-  promiseLibrary: require('bluebird'),
-  useUnifiedTopology: true,
-  useNewUrlParser: true
-}).then(() => {
-  console.debug(`Connection to the database instance was successful`);
-}).catch(err => {
-  console.log(`MongoDB Error: ${err.message}`)
-}); // end mongoose connection
-
+mongoose
+  .connect(process.env.DBCON, {
+    promiseLibrary: require("bluebird"),
+    useUnifiedTopology: true,
+    useNewUrlParser: true,
+  })
+  .then(() => {
+    console.debug(`Connection to the database instance was successful`);
+  })
+  .catch((err) => {
+    console.log(`MongoDB Error: ${err.message}`);
+  }); // end mongoose connection
 
 /**
  * API(s) go here
  */
 
- app.use('/api/employees', empRoutes);
-
+app.use("/api/employees", empRoutes);
 
 /**
  * Create and start server
  */
-http.createServer(app).listen(port, function() {
-  console.log(`Application started and listening on port: ${port}`)
+http.createServer(app).listen(port, function () {
+  console.log(`Application started and listening on port: ${port}`);
 }); // end http create server function
