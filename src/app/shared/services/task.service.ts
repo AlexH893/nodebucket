70e39bb80da30ef5dd026553a1a7a75d5e1a6cf2
Alex <31377763+AlexH893@users.noreply.github.com>
2021-11-13 12:32:57 -0700

    fixed updateTask API

--- src/app/shared/services/task.service.ts
+++ src/app/shared/services/task.service.ts
@@ -1,25 +1,31 @@
 import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
 import { Observable } from 'rxjs';
-
+import { Item } from '../models/item.interface';
 @Injectable({
-  providedIn: 'root'
+  providedIn: 'root',
 })
-
 export class TaskService {
-
-  constructor(private http: HttpClient) { }
+  constructor(private http: HttpClient) {}
 
   findAllTasks(empId: number): Observable<any> {
-
     return this.http.get('/api/employees/' + empId + '/tasks');
   }
 
   createTask(empId: number, task: string): Observable<any> {
-
     return this.http.post('/api/employees/' + empId + '/tasks', {
+      text: task,
+    });
+  }
 
-      text: task
-    })
-}
+  updateTask(empId: number, todo: Item[], done: Item[]): Observable<any> {
+    return this.http.put('/api/employees/' + empId + '/tasks', {
+      todo,
+      done,
+    });
+  }
+
+  deleteTask(empId: number, taskId: string): Observable<any> {
+    return this.http.delete('/api/employees/' + empId + '/tasks/' + taskId);
+  }
 }
