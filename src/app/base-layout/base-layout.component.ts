256b40ac4c9e68fa626bf3539ba06b8aa0e4b7b7
Alex <31377763+AlexH893@users.noreply.github.com>
2021-11-13 12:33:12 -0700

    added ability to change website theme

--- src/app/base-layout/base-layout.component.ts
+++ src/app/base-layout/base-layout.component.ts
@@ -1,23 +1,21 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, Inject } from '@angular/core';
 import { CookieService } from 'ngx-cookie-service';
 import { Router } from '@angular/router';
-
 
 @Component({
   selector: 'app-base-layout',
   templateUrl: './base-layout.component.html',
-  styleUrls: ['./base-layout.component.css']
+  styleUrls: ['./base-layout.component.css'],
 })
 export class BaseLayoutComponent implements OnInit {
-
   year: number = Date.now();
-
   // Define the assignment name
-  constructor(private cookieService: CookieService, private router: Router) {
-  }
 
-  ngOnInit(): void {
-  }
+  isXpTheme = false;
+
+  constructor(private cookieService: CookieService, private router: Router) {}
+
+  ngOnInit(): void {}
   //Add a new function named signOut()
   signOut() {
     //call the cookieService.deleteAll function and then use the Router to navigate users to the sign-in page
@@ -25,4 +23,21 @@ export class BaseLayoutComponent implements OnInit {
     this.router.navigate(['/session/sign-in']);
   }
 
+  changeTheme(): void {
+    if (this.isXpTheme) {
+      document
+        .getElementById('global-theme')
+        .setAttribute('href', '../../assets/styles/styles.css');
+      this.isXpTheme = false;
+    } else {
+      document
+        .getElementById('global-theme')
+        .setAttribute('href', '../../assets/styles/xp.css');
+      this.isXpTheme = true;
+    }
+  }
+
+  alert() {
+    alert('Coming soon');
+  }
 }
